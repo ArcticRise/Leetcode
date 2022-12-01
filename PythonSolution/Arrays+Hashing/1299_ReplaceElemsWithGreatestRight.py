5f29ddd4ae367f875137317e90f005ebf4b0bd48
mvo39 <mvo39@gatech.edu>
2022-11-30 22:45:24 -0500

    Changed 1299_ReplaceElem

--- PythonSolution/Arrays+Hashing/1299_ReplaceElemsWithGreatestRight.py
+++ PythonSolution/Arrays+Hashing/1299_ReplaceElemsWithGreatestRight.py
@@ -29,4 +29,26 @@ print(replaceElements(Case1))
 """
 O(n) time and space complexity as we loop through each elem and put into new array
 
+
+This could also be done without extra space!
+
+ie keep track of cur_max and just swap
+
+see below replaceElem2
+
 """
+
+
+def replaceElements2(arr: List[int]) -> List[int]:
+    cur_max = -1
+    for n in range(len(arr)-1,-1,-1):
+        temp = arr[n]
+        arr[n] = cur_max
+        if temp > cur_max:
+            cur_max = temp
+    return arr
+
+
+Case1 = [17,18,5,4,6,1]
+
+print(replaceElements2(Case1))
