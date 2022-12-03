e10eb8a67cb0d0567e9070dc43e6e2849341ff8d
mvo39 <mvo39@gatech.edu>
2022-12-02 23:36:06 -0500

    Solved 48_RotateImage

---
+++ PythonSolution/Arrays+Hashing/48_rotateImage.py
@@ -0,0 +1,24 @@
+"""
+You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+
+You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+
+"""
+from typing import List
+
+def rotate( matrix: List[List[int]]) -> None:
+    """
+    Do not return anything, modify matrix in-place instead.
+    """
+
+    #reverse then transpose
+    matrix.reverse()
+    for i in range(len(matrix)):
+        for j in range(i):
+            matrix[i][j],matrix[j][i] = matrix[j][i],matrix[i][j]
+
+
+case1 = [[1,2,3],[4,5,6],[7,8,9]]
+rotate(case1)
+print(case1)
+        
