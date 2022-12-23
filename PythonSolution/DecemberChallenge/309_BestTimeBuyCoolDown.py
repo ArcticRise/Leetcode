23090d46058ca1ae34bb307d048b7154a96e652f
mvo39 <mvo39@gatech.edu>
2022-12-22 22:32:01 -0500

    309 Best Time Buy Sell

--- PythonSolution/DecemberChallenge/309_BestTimeBuyCoolDown.py
+++ PythonSolution/DecemberChallenge/309_BestTimeBuyCoolDown.py
@@ -17,14 +17,15 @@ Explanation: transactions = [buy, sell, cooldown, buy, sell]
 from typing import List
 
 def maxProfit( prices: List[int]) -> int:
-    buy, sell, hold = inf, 0, 0
+    buy, sell, hold = float("inf"), 0, 0
     for p in prices:
         buy = min(buy, p - hold)
         hold = sell
         sell = max(sell, p - buy)
     return sell
 
-
+prices = [1,2,3,0,2]
+print(maxProfit(prices))
 """
 Time : O(N)
 Space: O(1)
